56d763bd9260233bb9e3ba84568a38dd0932b33e
Rafael Cunha <rafael_pbc@yahoo.com.br>
2021-06-26 16:12:58 -0300

    Criando o header principal

--- minha-carteira/src/components/MainHeader/index.tsx
+++ minha-carteira/src/components/MainHeader/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { Container } from './styles'
+import React , { useMemo } from "react";
+import emojis from "../../utils/emojis";
+
+import { Container, Profile, Welcome, UserName } from './styles'
+
 const MainHeader: React.FC = () => {
+
+   const emoji = useMemo(()=> {
+      const indece = Math.floor(Math.random() * emojis.length )
+      return emojis[indece];
+   }, [])
    return(
       <Container>
-         <h1>MainHeader</h1>
+         <h1>Toogle</h1>
+         <Profile>
+            <Welcome>Olá, {emoji}</Welcome>
+            <UserName>Rafael Cunha</UserName>
+         </Profile>
       </Container>
    )
 }
